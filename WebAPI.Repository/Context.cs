7f972e47af19ed600f6405add81f487fe342658b
Herbert Carvalho <hscarvalho@outlook.com.br>
2021-12-27 22:48:35 -0300

    Criação dos novos projetos

---
+++ WebAPI.Repository/Context.cs
@@ -0,0 +1,30 @@
+﻿ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Domain;
+
+namespace WebAPI.Repository
+{
+    public class Context : IdentityDbContext<User>
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserRole>();
+
+            builder.Entity<Organizacao>(org =>
+            {
+                org.ToTable("Organizacoes");
+                org.HasKey(x => x.Id);
+
+                org.HasMany<User>()
+                    .WithOne()
+                    .HasForeignKey(x => x.OrgId)
+                    .IsRequired(false);
+            });
+        }
+    }
+}
---
+++ WebApi.Domain/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApi.Domain
+{
+    public class User : IdentityUser
+    {
+        public string? NomeCompleto { get; set; }
+        public string Member { get; set; } = "Member";
+        public string? OrgId { get; set; }
+
+        public List<UserRole> UserRoles { get; set; }
+    }
+}
