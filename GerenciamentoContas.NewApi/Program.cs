0474318b6f78487802dc175e44e537a5f4c088c4
Herbert Carvalho <hscarvalho@outlook.com.br>
2022-01-04 16:58:47 -0300

    Criado controller

--- GerenciamentoContas.NewApi/Program.cs
+++ GerenciamentoContas.NewApi/Program.cs
@@ -1,3 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Reflection;
+using System.Text;
+using WebApi.Domain;
+using WebAPI.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,46 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var migrationAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<Context>(options => options.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationAssembly)));
+
+builder.Services.AddIdentity<User, Role>(options =>
+ {
+     options.SignIn.RequireConfirmedEmail = true;
+     options.Password.RequireDigit = false;
+     options.Password.RequireNonAlphanumeric = false;
+     options.Password.RequireLowercase = false;
+     options.Password.RequireUppercase = false;
+     options.Password.RequiredLength = 8;
+
+     options.Lockout.MaxFailedAccessAttempts = 3;
+     options.Lockout.AllowedForNewUsers = true;
+
+ })
+    .AddEntityFrameworkStores<Context>()
+    .AddRoleValidator<RoleValidator<Role>>()
+    .AddRoleManager<RoleManager<Role>>()
+    .AddSignInManager<SignInManager<User>>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddCors();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+            .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+
+
 
 var app = builder.Build();
 
@@ -15,7 +65,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.UseAuthorization();
 
 app.MapControllers();
